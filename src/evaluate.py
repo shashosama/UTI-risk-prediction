bfe1dd2bc0dccfd084b1b61cb5fe4fa07f4427da
shashosama <160487546+shashosama@users.noreply.github.com>
2025-06-16 22:22:23 +0000

    evaluate

--- src/evaluate.py
+++ src/evaluate.py
@@ -1,7 +1,7 @@
 from sklearn.metrics import classification_report, roc_auc_score, confusion_matrix, precision_recall_curve
 import matplotlib.pyplot as plt
 import seaborn as sns
-
+import shap
 def evaluate_model(model, X_test, y_test):
     y_pred = model.predict(X_test)
     print(classification_report(y_test, y_pred))
@@ -14,3 +14,12 @@ def evaluate_model(model, X_test, y_test):
     plt.plot(rec, prec)
     plt.title("Precision-Recall Curve")
     plt.show()
+def explain_model(model, X_sample):
+    # If pipeline: get model inside
+    clf = model.named_steps['randomforestclassifier'] if 'randomforestclassifier' in model.named_steps else model
+    
+    explainer = shap.TreeExplainer(clf)
+    shap_values = explainer.shap_values(X_sample)
+    
+    # Summary plot
+    shap.summary_plot(shap_values[1], X_sample)  
